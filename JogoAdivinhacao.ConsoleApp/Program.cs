7907badc7921f7bce67ed810904a6847ff878210
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-03-25 23:17:12 -0300

    Adicionado requisito de seleção de dificuldade

--- JogoAdivinhacao.ConsoleApp/Program.cs
+++ JogoAdivinhacao.ConsoleApp/Program.cs
@@ -1,39 +1,84 @@
 ﻿using System.Security.Cryptography;
 
-int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
-
 bool jogoDeveContinuar = true;
 
 while (true)
 {
     Console.Clear();
-    Console.WriteLine("-------------------");
-    Console.WriteLine("Jogo de Adivinhação");
-    Console.WriteLine("-------------------");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("-----  Jogo de Adivinhação  -----");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("-Escolha o nível de dificuldade:-");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("--  1 - Fácil (10 tentativas)  --");
+    Console.WriteLine("--  2 - Médio (5 tentativas)   --");
+    Console.WriteLine("--  3 - Dificil (3 tentativas) --");
 
-    Console.WriteLine();
-    Console.WriteLine("Digite um número: ");
-    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("------ Digite sua escolha: ------");
+    string dificuldadeEscolhida = Console.ReadLine();
 
-    if (numeroDigitado == numeroAleatorio)
+    int numeroAleatorio = 0;
+    int tentativasMaximas;
+
+    switch (dificuldadeEscolhida)
     {
-        Console.WriteLine("---------------------------");
-        Console.WriteLine("Boa mula acertou na cagada!");
-        Console.WriteLine("---------------------------");
+        case "1":
+            numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+            tentativasMaximas = 10;
+            break;
+
+        case "2":
+            numeroAleatorio = RandomNumberGenerator.GetInt32(1, 51);
+            tentativasMaximas = 5;
+            break;
+
+        case "3":
+            numeroAleatorio = RandomNumberGenerator.GetInt32(1, 101);
+            tentativasMaximas = 3;
+            break;
+
+        default:
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Por favor, selecione uma difiuldade válida.");
+            Console.WriteLine("------  Digite ENTER para continuar.  ------");
+            Console.ReadLine();
+            continue;
     }
 
-    else if (numeroDigitado > numeroAleatorio)
+    for (int tentativaAtual = 1; tentativaAtual <= tentativasMaximas; tentativaAtual++)
     {
-        Console.WriteLine("-------------------------------------------------------------");
-        Console.WriteLine("Errou animal. O número digitado é MAIOR que o número secreto.");
-        Console.WriteLine("-------------------------------------------------------------");
-    }
+        Console.Clear();
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("------ Jogo de Adivinhação ------");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine($"------ Tentativa {tentativaAtual} de {tentativasMaximas}. --------");
+        Console.WriteLine("---------------------------------");
+        int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
-    else
-    {
-        Console.WriteLine("----------------------------------------------------------------");
-        Console.WriteLine("Ooo jegue, o número que tu digitou é MENOR que o número secreto.");
-        Console.WriteLine("----------------------------------------------------------------");
+        if (numeroDigitado == numeroAleatorio)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Boa mula acertou na cagada!");
+            Console.WriteLine("------- O numero era: " + numeroAleatorio + "------");
+            Console.WriteLine("---------------------------");
+            break;
+        }
+
+        else if (numeroDigitado > numeroAleatorio)
+        {
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Errou animal. O número digitado é MAIOR que o número secreto.");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+        else
+        {
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Ooo jegue, o número que tu digitou é MENOR que o número secreto.");
+            Console.WriteLine("----------------------------------------------------------------");
+        }
+        Console.WriteLine("-- Digite ENTER para continuar --");
+        Console.ReadLine();
     }
 
     Console.WriteLine();
