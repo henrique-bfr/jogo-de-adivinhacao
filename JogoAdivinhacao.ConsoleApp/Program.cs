657804464b8d408fd2f6f1a09c3cfb68e787a013
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-03-25 22:09:19 -0300

    Adicionada multiplas tentativas

--- JogoAdivinhacao.ConsoleApp/Program.cs
+++ JogoAdivinhacao.ConsoleApp/Program.cs
@@ -1,36 +1,47 @@
 ﻿using System.Security.Cryptography;
 
-Console.WriteLine("-------------------");
-Console.WriteLine("Jogo de Adivinhação");
-Console.WriteLine("-------------------");
-
-Console.WriteLine();
-Console.WriteLine("Digite um número: ");
-string strnumeroDigitado = Console.ReadLine();
-
-int numeroDigitado = Convert.ToInt32(Console.ReadLine());
-
 int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
 
-if (numeroDigitado == numeroAleatorio)
-{
-    Console.WriteLine("---------------------------");
-    Console.WriteLine("Boa mula acertou na cagada!");
-    Console.WriteLine("---------------------------");
-}
+bool jogoDeveContinuar = true;
 
-else if (numeroDigitado > numeroAleatorio)
+while (true)
 {
-    Console.WriteLine("-------------------------------------------------------------");
-    Console.WriteLine("Errou animal. O número digitado é MAIOR que o número secreto.");
-    Console.WriteLine("-------------------------------------------------------------");
-}
+    Console.Clear();
+    Console.WriteLine("-------------------");
+    Console.WriteLine("Jogo de Adivinhação");
+    Console.WriteLine("-------------------");
 
-else
-{
-    Console.WriteLine("----------------------------------------------------------------");
-    Console.WriteLine("Ooo jegue, o número que tu digitou é MENOR que o número secreto.");
-    Console.WriteLine("----------------------------------------------------------------");
-}
+    Console.WriteLine();
+    Console.WriteLine("Digite um número: ");
+    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
-Console.ReadLine();
+    if (numeroDigitado == numeroAleatorio)
+    {
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("Boa mula acertou na cagada!");
+        Console.WriteLine("---------------------------");
+    }
+
+    else if (numeroDigitado > numeroAleatorio)
+    {
+        Console.WriteLine("-------------------------------------------------------------");
+        Console.WriteLine("Errou animal. O número digitado é MAIOR que o número secreto.");
+        Console.WriteLine("-------------------------------------------------------------");
+    }
+
+    else
+    {
+        Console.WriteLine("----------------------------------------------------------------");
+        Console.WriteLine("Ooo jegue, o número que tu digitou é MENOR que o número secreto.");
+        Console.WriteLine("----------------------------------------------------------------");
+    }
+
+    Console.WriteLine();
+    Console.Write("Deseja continuar? (S/N)");
+    string opcaoContinuar = Console.ReadLine().ToUpper();
+
+    if (opcaoContinuar != "S")
+    {
+        jogoDeveContinuar = false;
+    }
+}
